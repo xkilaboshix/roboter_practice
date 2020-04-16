acd2392278eac9b9f479a7151fdd66723bf2f309
xkilaboshix <xkilaboshix@yahoo.co.jp>
2020-04-16 16:31:10 +0900

    up

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import roboter.controller.conversation
-roboter.controller.conversation.talk_about_restaurant()
+
+
+class MainError(Exception):
+    pass
+
+
+def main():
+    roboter.controller.conversation.talk_about_restaurant()
+    raise MainError('error')
+
+if __name__ == '__main__':
+    main()
+
